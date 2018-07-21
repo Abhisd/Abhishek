4a6c1837ead793257b9a5eb9178132d7cb75ab42
Abhishek Dawange <37938117+Abhisd@users.noreply.github.com>
2018-07-21 19:25:23 +0530

    Update socket.py

--- socket.py
+++ socket.py
@@ -6,21 +6,21 @@ print("3. Get the host name")
 print("Enter your choice")
 ch=int(input())
 if ch==1:
-	urlname=input("Enter the Url")
+	urlname=raw_input("Enter the Url")
 	IPadder=socket.gethostbyname(urlname)
 	print(urlname) 
 	print(IPadder) 
 	
 
 elif ch==2:
-	ipadd=input("Enter the IP address")
+	ipadd=raw_input("Enter the IP address")
 	url=socket.gethostbyaddr(ipadd)
 	print(ipadd)
 	print(url)
 	
 elif ch==3:
 	print("    The host name is")
-	ipa=input("Enter the IP of your device")
+	ipa=raw_input("Enter the IP of your device")
 	name=socket.gethostname()
 	print(ipa) 
 	print(name)
